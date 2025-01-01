1ebc78b42e50e5690a00df70d586a3d65fcf0342
dell <dell@DESKTOP-4P8U8AR>
2025-01-01 19:16:22 +0100

    initilizing project , adding main dependenciesÃ

---
+++ src/main/java/com/ilyass/wsmulticonnectorspringsecurityjwt/WsMultiConnectorSpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.ilyass.wsmulticonnectorspringsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsMultiConnectorSpringSecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsMultiConnectorSpringSecurityJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ilyass/wsmulticonnectorspringsecurityjwt/WsMultiConnectorSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilyass.wsmulticonnectorspringsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsMultiConnectorSpringSecurityJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
